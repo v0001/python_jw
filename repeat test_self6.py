9a8d5bf2680eccc533c7793ece836a2642380e5e
v0001 <guswjd6005@gmail.com>
2020-06-15 17:29:53 +0900

    initial commit

---
+++ repeat test_self6.py
@@ -0,0 +1,18 @@
+while(True):
+    print("1. Korea")
+    print("2. USA")
+    print("3. Japan")
+    print("4. China")
+    a = int(input("number?  "))
+
+    if a == 1:
+        print("\nSeoul \n")
+    elif a == 2:
+        print("\nWashington \n")
+    elif a == 3:
+        print("\nTokyo \n")
+    elif a == 4:
+        print("\nBeijing \n")
+    else:
+        print("\nnone")
+        break
