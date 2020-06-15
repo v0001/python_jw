7ff42f06c0532ce52a293ef7d30a89b7544e0561
v0001 <guswjd6005@gmail.com>
2020-06-15 18:56:18 +0900

    update

--- repeat test_exam2.py
+++ repeat test_exam2.py
@@ -1,2 +1,26 @@
-# 정수를 입력받아 1부터 입력받은 정수까지를 차례대로 출력하는 프로그램을 작성하시오.
+# 0 이상의 정수들이 공백으로 구분되어 반복적으로 주어진다.
 
+# 0이 입력되면 반복문을 멈추고 그 전까지 입력받은 수들에 대하여
+
+# 홀수의 개수와 짝수의 개수를 출력하는 프로그램을 작성하시오.
+i =0
+a = input().strip().split(" ")
+b =[]
+cnt_odd = 0
+cnt_even = 0
+
+for i in range(0,len(a)):
+    if a[i] == '0':
+        break
+    else:
+        b.append(int(a[i]))
+        continue
+
+for i in range(0,len(b)):
+    if b[i] % 2 == 0:
+        cnt_even = cnt_even + 1
+    else:
+        cnt_odd = cnt_odd + 1
+
+print('odd :',  cnt_odd)
+print('even :',  cnt_even)
---
+++ repeat test_exam3.py
@@ -0,0 +1,22 @@
+# 0 부터 100 까지의 점수를 계속 입력받다가 범위를 벗어나는 수가 입력되면 그 이전까지 입력된 자료의 합계와 평균을 출력하는 프로그램을 작성하시오.
+
+# (평균은 반올림하여 소수 첫째자리까지 출력한다.)
+
+
+i =0
+a = input().strip().split(" ")
+b =[]
+total = 0
+
+for i in range(0,len(a)):
+    if int(a[i]) >100:
+        break
+    else:
+        b.append(int(a[i]))
+        continue
+
+for i in range(0,len(b)):
+    total = total + b[i]
+
+print('sum :', total)
+print('total :', round(total/len(b),1))
